93d807965d83ae48e2ca04d47676f2236cf88eb4
Ivan Saina <ivansaina@hotmail.com>
2021-07-01 21:23:43 -0300

    ACA VA LA CLASE VENTA

---
+++ GitEjercicio1/src/gitejercicio1/Venta.java
@@ -0,0 +1,16 @@
+package gitejercicio1;
+
+public class Venta {
+
+    private String producto;
+    private Integer Precio;
+    private int cantidad;
+
+    public void venta() {
+    }
+
+    public void registroVenta() {
+
+    }
+
+}
